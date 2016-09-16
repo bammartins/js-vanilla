290ebd16ee9e895b45bb256028006aaeda469979
Bruno Martins <bruno.martins@wunderman.com>
2016-09-16 17:36:06 -0300

    ajuste no uso do prototype js

--- rotas/script/routes.js
+++ rotas/script/routes.js
@@ -1,66 +1,87 @@
 Routes = function(){
 			
-			function Routes(){
-					this.type,
-					this.state,
-					this.url,
-					this.template,
-					this.isModal,
+			function Routes(sType, sState, sUrl, sTemplate, bModal, sIcon, sSize){
+					this.type = sType;
+					this.state = sState;
+					this.url = sUrl;
+					this.template = sTemplate;
+					this.isModal = bModal;
+					this.icon = sIcon;
+					this.sizeModal = sSize;
 					this.routeIds = []; 				
 			}	
 
-			// Routes.prototype.setAttr = function(element, attributes){
-			// 	  for(var key in attributes) {
-			// 	    element.setAttribute(key, attributes[key]);
-			// 	  }
+			// //Recebe o elemento pai e um array com o tipo do elemento que deseja se criar 
+			// Routes.prototype.AppendEl = function (parent, elements){
+			// 	// Array com os placeholders dos inputs criados dinamicamente
+			// 	var labelNames = ['State', 'Url', 'Template'];
+			// 	// Percorre o array, cria o elemento e faz o append dele no parent
+			// 	for (var i = 0; i < elements.length; i++) {
+			// 		var el = document.createElement(elements[i]);
+			// 		el.setAttribute('class', "iText");
+			// 		el.setAttribute('type', "text");
+			// 		el.setAttribute('placeholder', labelNames[i]);
+			// 		parent.appendChild(el);
+			// 	}				
 			// }
 
-			Routes.prototype.AppendEl = function (parent, elements){
-				var labelNames = ['State', 'Url', 'Template'];
-				for (var i = 0; i < elements.length; i++) {
-					var el = document.createElement(elements[i]);
-					el.setAttribute('id', i+"-input");
-					el.setAttribute('class', "iText");
-					el.setAttribute('type', "text");
-					el.setAttribute('placeholder', labelNames[i]);
-					parent.appendChild(el);
-				}				
-			}
-
-			Routes.prototype.ValidateForm = function (){
-				var inputs 	= document.getElementsByClassName('iText');					
-				for (var i = 0; i < inputs.length; i++) {
-					if(inputs[i].value != ""){
-						this.routeIds.push(inputs[i].value);
-					}else{
-						console.log('preenche sapoha');
-						return;
-					}
-				}
-				return this.routeIds;
-			}
+			// // Valida o valor do formulario
+			// Routes.prototype.ValidateForm = function (){
+			// 	var inputs 	= document.getElementsByClassName('iText');					
+			// 	for (var i = 0; i < inputs.length; i++) {
+			// 		if(inputs[i].value != ""){
+			// 			this.routeIds.push(inputs[i].value);
+			// 		}else{
+			// 			console.log('preenche sapoha');
+			// 			return;
+			// 		}
+			// 	}
+			// 	return this.routeIds;
+			// }
 			
-			Routes.prototype.BuildRoute = function(sType, sState, sUrl, sTemplate, bModal){				
+			// Constroi e retorna a rota
+			Routes.prototype.BuildRoute = function(){				
 				var newRoute;
-				this.type = sType;
-				this.state = sState;
-				this.url = sUrl;
-				this.template = sTemplate;
-				this.isModal = bModal;
 
-				if (bModal === undefined) {
+				if (this.isModal === undefined) {
 					newRoute = ".state('"+this.type+"."+this.state+"', {<br> &nbsp &nbsp url: '"+this.url+"',<br> &nbsp &nbsp templateUrl: '"+this.template+"' <br>})";
 				}else{
-					newRoute = ".state('"+this.type+"."+this.state+"', {<br> &nbsp &nbsp url: '"+this.url+"',<br> &nbsp &nbsp templateUrl: '"+this.template+"', <br> &nbsp &nbsp modalView : {icon : '', size : 'md'}<br>})";
+					newRoute = ".state('"+this.type+"."+this.state+"', {<br> &nbsp &nbsp url: '"+this.url+"',<br> &nbsp &nbsp templateUrl: '"+this.template+"', <br> &nbsp &nbsp modalView : {icon : '"+this.icon+"', size : '"+this.sizeModal+"'}<br>})";
 				}
 				return newRoute;
 			}
 			return Routes;
 		}();
-		
+function AppendEl(parent, elements){
+	// Array com os placeholders dos inputs criados dinamicamente
+	var labelNames = ['State', 'Url', 'Template'];
+	// Percorre o array, cria o elemento e faz o append dele no parent
+	for (var i = 0; i < elements.length; i++) {
+		var el = document.createElement(elements[i]);
+		el.setAttribute('class', "iText");
+		el.setAttribute('type', "text");
+		el.setAttribute('placeholder', labelNames[i]);
+		parent.appendChild(el);
+	}				
+}	
+
+function ValidateForm(){
+	var inputs 	= document.getElementsByClassName('iText'),
+		arrayValues = [];					
+	for (var i = 0; i < inputs.length; i++) {
+		if(inputs[i].value != ""){
+			arrayValues.push(inputs[i].value);
+		}else{
+			console.log('preenche sapoha');
+			return;
+		}
+	}
+	return arrayValues;
+}	
+
 function triggerEvent(){
-	var newRoute = new Routes();
-	var state,
+	var newRoute,
+		state,
 		url,
 		template,
 		radios 		= document.forms["formRoute"].elements["type"],
@@ -70,13 +91,12 @@ function triggerEvent(){
 
 	for(radio in radios) {
 	    radios[radio].onclick = function(e) {
-			var	formRoute 	   = document.getElementById("formulario"),
-				buttonGenerate = document.getElementById('generateRoute');
+			var	formRoute 	   = document.getElementById("formulario");
 
 			for (var i = 0; i < radioButton.length; i++) {
 				if(radioButton[i].checked){
-					newRoute.AppendEl(formRoute, ['input','input','input']);
-					buttonGenerate.removeAttribute("disabled");							
+					AppendEl(formRoute, ['input','input','input']);
+					finishRoute.removeAttribute("disabled");							
 				}
 			}					
 	    }
@@ -84,7 +104,7 @@ function triggerEvent(){
 
 	finishRoute.onclick = function(e){
 		e.preventDefault();
-			var routeStep   = newRoute.ValidateForm(),
+			var routeStep   = ValidateForm(),
 				routeDiv    = document.getElementById('routeScript'),
 				checkedBox  = document.getElementById('checkbox'),
 				script;
@@ -101,15 +121,15 @@ function triggerEvent(){
 			}
 
 			if (checkedBox.checked) {
-				script = newRoute.BuildRoute(radioValue, state, url, template, true);
+				newRoute = new Routes(radioValue, state, url, template, true);
+				script = newRoute.BuildRoute();
 			}else{
+				newRoute = new Routes(radioValue, state, url, template);
 				script = newRoute.BuildRoute(radioValue, state, url, template);						
 			}
 			routeDiv.innerHTML = script;
 	}
 }
-			
-
 
 window.onload = function(){
 	triggerEvent();
